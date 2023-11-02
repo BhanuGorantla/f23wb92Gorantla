97b82686432b81e90f70654ad4d7ae16b61b3746
BhanuGorantla <143116800+BhanuGorantla@users.noreply.github.com>
2023-11-01 21:37:56 -0500

    updated all

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bikes', bikesRouter);
-app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
